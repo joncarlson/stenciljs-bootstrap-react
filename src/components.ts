d9cea41bf33536759c8fb27e63be73a7f4b6c93c
Jon Carlson <jdcarlso@gs6102dscmcjdca.ndc.nasa.gov>
2021-03-17 16:32:46 -0400

    Initial commit

---
+++ src/components.ts
@@ -0,0 +1,44 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'stenciljs-bootstrap';
+
+
+
+export const BsAlert = /*@__PURE__*/createReactComponent<JSX.BsAlert, HTMLBsAlertElement>('bs-alert');
+export const BsBadge = /*@__PURE__*/createReactComponent<JSX.BsBadge, HTMLBsBadgeElement>('bs-badge');
+export const BsBreadcrumb = /*@__PURE__*/createReactComponent<JSX.BsBreadcrumb, HTMLBsBreadcrumbElement>('bs-breadcrumb');
+export const BsBreadcrumbs = /*@__PURE__*/createReactComponent<JSX.BsBreadcrumbs, HTMLBsBreadcrumbsElement>('bs-breadcrumbs');
+export const BsButton = /*@__PURE__*/createReactComponent<JSX.BsButton, HTMLBsButtonElement>('bs-button');
+export const BsButtonGroup = /*@__PURE__*/createReactComponent<JSX.BsButtonGroup, HTMLBsButtonGroupElement>('bs-button-group');
+export const BsCard = /*@__PURE__*/createReactComponent<JSX.BsCard, HTMLBsCardElement>('bs-card');
+export const BsCardBody = /*@__PURE__*/createReactComponent<JSX.BsCardBody, HTMLBsCardBodyElement>('bs-card-body');
+export const BsCardFooter = /*@__PURE__*/createReactComponent<JSX.BsCardFooter, HTMLBsCardFooterElement>('bs-card-footer');
+export const BsCardHeader = /*@__PURE__*/createReactComponent<JSX.BsCardHeader, HTMLBsCardHeaderElement>('bs-card-header');
+export const BsCardImage = /*@__PURE__*/createReactComponent<JSX.BsCardImage, HTMLBsCardImageElement>('bs-card-image');
+export const BsCarousel = /*@__PURE__*/createReactComponent<JSX.BsCarousel, HTMLBsCarouselElement>('bs-carousel');
+export const BsCollapse = /*@__PURE__*/createReactComponent<JSX.BsCollapse, HTMLBsCollapseElement>('bs-collapse');
+export const BsDropdown = /*@__PURE__*/createReactComponent<JSX.BsDropdown, HTMLBsDropdownElement>('bs-dropdown');
+export const BsFormCheck = /*@__PURE__*/createReactComponent<JSX.BsFormCheck, HTMLBsFormCheckElement>('bs-form-check');
+export const BsFormCheckGroup = /*@__PURE__*/createReactComponent<JSX.BsFormCheckGroup, HTMLBsFormCheckGroupElement>('bs-form-check-group');
+export const BsFormControl = /*@__PURE__*/createReactComponent<JSX.BsFormControl, HTMLBsFormControlElement>('bs-form-control');
+export const BsFormFile = /*@__PURE__*/createReactComponent<JSX.BsFormFile, HTMLBsFormFileElement>('bs-form-file');
+export const BsFormGroup = /*@__PURE__*/createReactComponent<JSX.BsFormGroup, HTMLBsFormGroupElement>('bs-form-group');
+export const BsFormLabel = /*@__PURE__*/createReactComponent<JSX.BsFormLabel, HTMLBsFormLabelElement>('bs-form-label');
+export const BsFormRow = /*@__PURE__*/createReactComponent<JSX.BsFormRow, HTMLBsFormRowElement>('bs-form-row');
+export const BsIcon = /*@__PURE__*/createReactComponent<JSX.BsIcon, HTMLBsIconElement>('bs-icon');
+export const BsListGroup = /*@__PURE__*/createReactComponent<JSX.BsListGroup, HTMLBsListGroupElement>('bs-list-group');
+export const BsListGroupItem = /*@__PURE__*/createReactComponent<JSX.BsListGroupItem, HTMLBsListGroupItemElement>('bs-list-group-item');
+export const BsModal = /*@__PURE__*/createReactComponent<JSX.BsModal, HTMLBsModalElement>('bs-modal');
+export const BsModalBody = /*@__PURE__*/createReactComponent<JSX.BsModalBody, HTMLBsModalBodyElement>('bs-modal-body');
+export const BsModalDialog = /*@__PURE__*/createReactComponent<JSX.BsModalDialog, HTMLBsModalDialogElement>('bs-modal-dialog');
+export const BsModalFooter = /*@__PURE__*/createReactComponent<JSX.BsModalFooter, HTMLBsModalFooterElement>('bs-modal-footer');
+export const BsModalHeader = /*@__PURE__*/createReactComponent<JSX.BsModalHeader, HTMLBsModalHeaderElement>('bs-modal-header');
+export const BsNav = /*@__PURE__*/createReactComponent<JSX.BsNav, HTMLBsNavElement>('bs-nav');
+export const BsNavItem = /*@__PURE__*/createReactComponent<JSX.BsNavItem, HTMLBsNavItemElement>('bs-nav-item');
+export const BsPageItem = /*@__PURE__*/createReactComponent<JSX.BsPageItem, HTMLBsPageItemElement>('bs-page-item');
+export const BsPagination = /*@__PURE__*/createReactComponent<JSX.BsPagination, HTMLBsPaginationElement>('bs-pagination');
+export const BsSpinner = /*@__PURE__*/createReactComponent<JSX.BsSpinner, HTMLBsSpinnerElement>('bs-spinner');
+export const BsToast = /*@__PURE__*/createReactComponent<JSX.BsToast, HTMLBsToastElement>('bs-toast');
